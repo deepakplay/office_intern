bf4b26fced498dcaab12ed69d9d24c8818d454ed
Deepak Kumar <75525675+deepakplay@users.noreply.github.com>
2021-02-03 13:56:24 +0530

    First_template

---
+++ site_1/assets/script.js
@@ -0,0 +1,27 @@
+'use strict';
+
+let menu_open = false;
+
+document.querySelector(".mobile_menu").addEventListener('click', (e)=>{
+	if(menu_open ===false){
+		document.querySelector(".main_header ul").style.display='block';
+		menu_open = true;
+	}
+});
+
+document.querySelector(".main_header .close_nav").addEventListener('click', (e)=>{
+	if(menu_open === true){
+		document.querySelector(".main_header ul").style.display='none';
+		menu_open = false;
+	}
+});
+
+window.addEventListener('resize', (e)=>{
+	if(e.target.outerWidth>=700){
+		console.log("Desktop Mode");
+		document.querySelector(".main_header ul").removeAttribute('style');
+		menu_open = false;
+	}else{
+		console.log("Mobile Mode");
+	};	
+})
